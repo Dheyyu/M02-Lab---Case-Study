537a0f7322c6701b7761e8c36c7c4e1294849654
Oladayo Ayorinde <oladayodavid5@gmail.com>
2024-06-17 18:58:15 -0400

    Updated the decriptions

--- M02_Lab.py
+++ M02_Lab.py
@@ -1,24 +1,24 @@
 # Author: Oladayo David Ayorinde
 # File name: M02_Lab.py
-# Description: This app determines if a student qualifies for the Dean's list or Honor Roll based on their GPA.
+# Description: This app determines if a student qualifies for the Dean's list or Honor Roll based on their GPA. The variables used are last_name (to store the last name of the student), first_name (to store the first name of the student), and GPA (to store the GPA of the student).
 
 def main():
     print ("Student Qualification App")
 
     while True:
         print()
-        # Asking for the student's last name
+        # Asking for the student's last name and storing it in the last_name variable
         last_name = input("Enter the student's last name: ")
         print()
         # This quits processing student records if the last name entered is 'ZZZ'
         if last_name == 'ZZZ':
             break
 
-        # Asking for the student's first name
+        # Asking for the student's first name and storing it in the first_name variable.
         first_name = input("Enter the student's first name: ")
         print()
         
-        # Asking for the student's GPA
+        # Asking for the student's GPA and storing it in the GPA variable
         gpa = float(input("Enter the student's GPA: "))
         print()
 
